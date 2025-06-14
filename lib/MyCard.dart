1340f244965fdedfdb3133e320bc8cc5d5cea956
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-06-14 13:30:23 +0500

    project done

--- lib/MyCard.dart
+++ lib/MyCard.dart
@@ -9,6 +9,7 @@ class Mycard extends StatelessWidget {
       backgroundColor: Colors.teal,
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 50.0,
@@ -22,6 +23,36 @@ class Mycard extends StatelessWidget {
             Text(
               'Flutter Developer',
               style: TextStyle(fontSize: 20.0, color: Colors.teal[900],fontFamily: 'Poppins '),
+            ),
+            SizedBox(
+              width: 250.0,
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+              padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Icon(Icons.email,color: Colors.teal,),
+                  SizedBox(width: 20.0,),
+                  Text('yar85224@gmail.com',style: TextStyle(fontSize: 20.0),)
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+              padding: EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Icon(Icons.phone,color: Colors.teal,),
+                  SizedBox(width: 20.0,),
+                  Text('03406311159',style: TextStyle(fontSize: 20.0),)
+                ],
+              ),
             )
           ],
         ),
