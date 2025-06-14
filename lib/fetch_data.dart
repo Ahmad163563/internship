588064b03b6b8231602f7746fad9b91cc5c4da00
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-06-14 20:14:17 +0500

    update

---
+++ lib/fetch_data.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+class FetchData extends StatefulWidget {
+  const FetchData({super.key});
+
+  @override
+  State<FetchData> createState() => _FetchDataState();
+}
+
+class _FetchDataState extends State<FetchData> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(stream: FirebaseFirestore.instance.collection('insert').snapshots(),
+
+        builder:(context,snapshots){
+      return ListView.builder(
+          itemCount: snapshots.data!.docs.length,
+          itemBuilder: (context,index){
+            
+          });
+        } );
+  }
+}
