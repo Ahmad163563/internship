b98d64ea8610ac8d6af422bf2eaafc5446bf7529
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-06-16 12:16:27 +0500

    wdwd

--- lib/firebase_auth/loading.dart
+++ lib/firebase_auth/loading.dart
@@ -1,9 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Loading_auth extends StatelessWidget {
+class Loading_auth extends StatefulWidget {
   Loading_auth({super.key});
-bool isLoading=false;
+
+  @override
+  State<Loading_auth> createState() => _Loading_authState();
+}
+
+class _Loading_authState extends State<Loading_auth> {
+  bool isLoading = false;
+
   TextEditingController emailController = TextEditingController();
 
   TextEditingController passwordController = TextEditingController();
@@ -33,15 +40,30 @@ bool isLoading=false;
                 keyboardType: TextInputType.emailAddress,
               ),
             ),
-            isLoading?CircularProgressIndicator(),
-            TextButton(
-                onPressed: () async {
-                  
-                },
-                child: Text(
-                  'signup',
-                  style: TextStyle(color: Colors.white),
-                ))
+            isLoading
+                ? CircularProgressIndicator()
+                : TextButton(
+                    onPressed: () async {
+                      isLoading = true;
+                      setState(() {});
+                      await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: emailController.text.trim(),
+                              password: passwordController.text)
+                          .then((onValue) {
+                        isLoading = false;
+                        setState(() {});
+                      }).onError((handleError,error){
+                        isLoading=false;
+                        setState(() {
+
+                        });
+                      });
+                    },
+                    child: Text(
+                      'signup',
+                      style: TextStyle(color: Colors.white),
+                    ))
           ],
         ),
       ),
