e495921c3b904c0d7035b2845857222481afd814
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-06-19 13:17:58 +0500

    oedjodfjiwj

---
+++ lib/ai_proposal_writing/HomeScreen2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen2 extends StatelessWidget {
+  const HomeScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Step 2/3',style: TextStyle(color: Colors.black,fontWeight: FontWeight.w700,fontSize:30),),
+      ),
+    );
+  }
+}
--- lib/ai_proposal_writing/peoposal type.dart
+++ lib/ai_proposal_writing/peoposal type.dart
@@ -1,7 +1,19 @@
+import 'package:firebasedemo/ai_proposal_writing/HomeScreen2.dart';
 import 'package:flutter/material.dart';
 
 class Proposalclass extends StatelessWidget {
-  const Proposalclass({super.key});
+   Proposalclass({super.key});
+  final List<String> items = [
+    ' Business Proposal',
+    'Project Proposal',
+    'Freelance Proposal',
+    'Partnership Proposal',
+    'Grant Proposal',
+    'Research Proposal',
+    'Sponsorship Proposal',
+    'Custom Proposal',
+
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -13,22 +25,39 @@ class Proposalclass extends StatelessWidget {
          Column(
           children: [
             
-            Text('Select Proposal Type',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20),),
-            ElevatedButton.icon(
-              onPressed: () {},
-              label: Text('Business Proposal',style: TextStyle(
-                  color: Colors.white
-              ),),
-              icon: Icon(Icons.arrow_forward_ios_sharp,color: Colors.white,),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                minimumSize: Size.fromHeight(60),
-                textStyle: TextStyle(fontSize: 18),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+            Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Text('Select Proposal Type',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20),)),
+      Expanded(
+        child: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.all(8.0),
+              child: InkWell(
+                onTap: () {
+                  // Jab item pe click ho to dusra screen open karo
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen2()));
+                },
+                child: Container(
+                  height: 50,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Padding(padding: EdgeInsets.symmetric(vertical: 9,horizontal: 5),
+                    child: Text(
+                      items[index],
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
                 ),
               ),
-            ),
+            );
+          },
+        ),
+      ),
           ],
         ),
       );
