1a730b688e95be3d5aca2ef0dc9a3fdab2515f65
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-06-20 11:48:20 +0500

    xzx

--- lib/ai_proposal_writing/HomeScreen2.dart
+++ lib/ai_proposal_writing/HomeScreen2.dart
@@ -16,69 +16,71 @@ class HomeScreen2 extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(vertical: 20,horizontal: 20),
-        child: Column(
-
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(proposalName,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.blue), ),
-            SizedBox(height: 20,width: 35,),
-            Column(
-              children:[ Container(
+        child: SingleChildScrollView(
+          child: Column(
+          
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(proposalName,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.blue), ),
+              SizedBox(height: 20,width: 35,),
+              Column(
+                children:[ Container(
+                  height: 120,   // 👈 Height set ki
+                  width: 400,   // 👈 Width set ki
+                   // 👈 Inner padding
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 15,bottom: 15,top: 15,),
+                      border: InputBorder.none,
+                      hintText: 'Optional: My Information',
+                    ),
+                  ),
+                ),
+          ]
+              ),
+              SizedBox(height: 20,width: 20,),
+              Container(
                 height: 120,   // 👈 Height set ki
                 width: 400,   // 👈 Width set ki
-                 // 👈 Inner padding
+                // 👈 Inner padding
                 decoration: BoxDecoration(
                   color: Colors.grey[300],
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: TextField(
                   decoration: InputDecoration(
-                    contentPadding: EdgeInsets.only(left: 15,bottom: 15,top: 15,),
+                    contentPadding: EdgeInsets.only(left: 15,bottom: 15,top: 15),
                     border: InputBorder.none,
-                    hintText: 'Optional: My Information',
+                    hintText: 'Optional: Prospect Info',
                   ),
                 ),
               ),
-        ]
-            ),
-            SizedBox(height: 20,width: 20,),
-            Container(
-              height: 120,   // 👈 Height set ki
-              width: 400,   // 👈 Width set ki
-              // 👈 Inner padding
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextField(
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(left: 15,bottom: 15,top: 15),
-                  border: InputBorder.none,
-                  hintText: 'Optional: Prospect Info',
-                ),
-              ),
-            ),
-            SizedBox(height: 20,),
-              TextButton(
-                onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen3(proposalName: proposalName,)));},
-                child: Container(
-                height: 50,
-                width: 350,
-                decoration: BoxDecoration(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.circular(19)
-                ),
-                child:
-                Center(
-                  child: Text('Continue',style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 10
-                  ),)
-                ),
-                         ),
-              )
-          ],
+              SizedBox(height: 20,),
+                TextButton(
+                  onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen3(proposalName: proposalName,)));},
+                  child: Container(
+                  height: 50,
+                  width: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(19)
+                  ),
+                  child:
+                  Center(
+                    child: Text('Continue',style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 10
+                    ),)
+                  ),
+                           ),
+                )
+            ],
+          ),
         ),
       )
     );
--- lib/ai_proposal_writing/homeScreen3.dart
+++ lib/ai_proposal_writing/homeScreen3.dart
@@ -29,149 +29,151 @@ class _HomeScreen3State extends State<HomeScreen3> {
             style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30),
           ),
         ),
-        body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Text(
-            widget.proposalName,
-            style: TextStyle(
-                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.blue),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Column(children: [
-              Container(
-                height: 120, // 👈 Height set ki
-                width: 400, // 👈 Width set ki
-                // 👈 Inner padding
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(10),
+        body: SingleChildScrollView(
+          child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            Text(
+              widget.proposalName,
+              style: TextStyle(
+                  fontSize: 25, fontWeight: FontWeight.bold, color: Colors.blue),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Column(children: [
+                Container(
+                  height: 120, // 👈 Height set ki
+                  width: 400, // 👈 Width set ki
+                  // 👈 Inner padding
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(
+                          left: 15,
+                          bottom: 15,
+                          top: 15,
+                        ),
+                        border: InputBorder.none,
+                        hintText: 'What is the purpose of this proposal',
+                      ),
+                    ),
+                  ),
                 ),
-                child: Center(
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: 120, // 👈 Height set ki
+                  width: 400, // 👈 Width set ki
+                  // 👈 Inner padding
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                   child: TextField(
                     decoration: InputDecoration(
-                      contentPadding: EdgeInsets.only(
-                        left: 15,
-                        bottom: 15,
-                        top: 15,
-                      ),
+                      contentPadding:
+                          EdgeInsets.only(left: 15, bottom: 15, top: 15),
                       border: InputBorder.none,
-                      hintText: 'What is the purpose of this proposal',
+                      hintText: 'Optional: Additional Information',
                     ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 120, // 👈 Height set ki
-                width: 400, // 👈 Width set ki
-                // 👈 Inner padding
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(10),
+                SizedBox(
+                  height: 20,
                 ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    contentPadding:
-                        EdgeInsets.only(left: 15, bottom: 15, top: 15),
-                    border: InputBorder.none,
-                    hintText: 'Optional: Additional Information',
+                Container(
+                  height: 80, // 👈 Height set ki
+                  width: 400, // 👈 Width set ki
+                  // 👈 Inner padding
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
                   ),
+                  child: DropdownButtonHideUnderline(
+                      child: DropdownButton(
+                          value: selectedOption == "Select an option"
+                              ? null
+                              : selectedOption,
+                          hint: Text("Select an option"),
+                          icon: Icon(Icons.arrow_drop_down),
+                          isExpanded: true,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedOption = newValue!;
+                            });
+                          },
+                          items: options.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList())),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                height: 80, // 👈 Height set ki
-                width: 400, // 👈 Width set ki
-                // 👈 Inner padding
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(10),
+                SizedBox(
+                  height: 10,
                 ),
-                child: DropdownButtonHideUnderline(
-                    child: DropdownButton(
-                        value: selectedOption == "Select an option"
-                            ? null
-                            : selectedOption,
-                        hint: Text("Select an option"),
-                        icon: Icon(Icons.arrow_drop_down),
-                        isExpanded: true,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            selectedOption = newValue!;
-                          });
-                        },
-                        items: options.map((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList())),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-
-                crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                Checkbox(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)),
-                  activeColor: Colors.red,
-                  value: option1,
-                  onChanged: ((value) {
-                    option1 = value!;
-                    setState(() {});
-                  }),
-                ),
-                Text('Standard Smart'),
                 Row(
-                  children: [
-                    Checkbox(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)),
-                      activeColor: Colors.red,
-                      value: option2,
-                      onChanged: ((value) {
-                        option2 = value!;
-                        setState(() {});
-                      }),
-                    ),
-                  ],
-                ),
-                Text('Standard Smart')
+          
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  Checkbox(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)),
+                    activeColor: Colors.red,
+                    value: option1,
+                    onChanged: ((value) {
+                      option1 = value!;
+                      setState(() {});
+                    }),
+                  ),
+                  Text('Standard Smart'),
+                  Row(
+                    children: [
+                      Checkbox(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50)),
+                        activeColor: Colors.red,
+                        value: option2,
+                        onChanged: ((value) {
+                          option2 = value!;
+                          setState(() {});
+                        }),
+                      ),
+                    ],
+                  ),
+                  Text('Standard Smart')
+                ]),
+          
               ]),
-
-            ]),
-          ),
-          SizedBox(height: 10,),
-          TextButton(
-            onPressed: (){},
-            child: Container(
-              height: 50,
-              width: 350,
-              decoration: BoxDecoration(
-                  color: Colors.deepPurple,
-                  borderRadius: BorderRadius.circular(19)
-              ),
-              child:
-              Center(
-                  child: Text('Continue',style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 10
-                  ),)
-              ),
             ),
-          )
-
-        ]),);
+            SizedBox(height: 10,),
+            TextButton(
+              onPressed: (){},
+              child: Container(
+                height: 50,
+                width: 350,
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    borderRadius: BorderRadius.circular(19)
+                ),
+                child:
+                Center(
+                    child: Text('Continue',style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 10
+                    ),)
+                ),
+              ),
+            )
+          
+          ]),
+        ),);
   }
 }
