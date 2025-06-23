ab9528026304e7f5e5f28caa42f1bb72ee2cc808
AHMAD <67407783+Ahmad163563@users.noreply.github.com>
2025-06-23 12:06:22 +0500

    working

--- lib/InsertScreen.dart
+++ lib/InsertScreen.dart
@@ -1,16 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Insertscreen extends StatefulWidget {
-  const Insertscreen({super.key});
+class InsertScreen extends StatefulWidget {
+  const InsertScreen({super.key});
 
   @override
-  State<Insertscreen> createState() => _InsertscreenState();
+  State<InsertScreen> createState() => _InsertScreenState();
 }
 
-class _InsertscreenState extends State<Insertscreen> {
-  TextEditingController titlecontroller=TextEditingController();
-  TextEditingController descriptioncontroller=TextEditingController();
+class _InsertScreenState extends State<InsertScreen> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
   bool isLoading=false;
   @override
   Widget build(BuildContext context) {
@@ -20,40 +20,71 @@ class _InsertscreenState extends State<Insertscreen> {
       ),
       body: Column(
         children: [
-          TextFieldWidget(controller: titlecontroller, hintText: 'Enter title',),
-          TextFieldWidget(controller: descriptioncontroller, hintText: 'Enter description',),
+          TextFieldWidget(
+            controller: titleController,
+            hintText: 'Enter title',
+          ),
+          TextFieldWidget(
+            controller: descriptionController,
+            hintText: 'Enter description',
+          ),
           isLoading?CircularProgressIndicator():
-          FloatingActionButton(onPressed: ()async{
-    try
-    {
-    isLoading=true;
-    setState(() {
+          FloatingActionButton(
+            onPressed: () async {
+              try
+              {
+                isLoading=true;
+                setState(() {
 
-    });
-    String docId=DateTime.now().microsecondsSinceEpoch.toString();
-    await FirebaseFirestore.instance.collection('insert').add({
-    'title': titlecontroller.text,
-    'description': descriptioncontroller.text,'docId':docId
-    });
+                });
+                 String docId=DateTime.now().microsecondsSinceEpoch.toString();
+                await FirebaseFirestore.instance.collection('insert').doc(docId).set({
+                  'title': titleController.text,
+                  'description':descriptionController.text,
+                  'docId':docId
+                });
+                isLoading=false;
+                titleController.clear();
+                descriptionController.clear();
+                setState(() {
 
-    isLoading=false;
-    titlecontroller.clear();
-    descriptioncontroller.clear();
-    setState(() {
+                });
+                Navigator.pop(context);
+              }
+              catch(error)
+              {
+                isLoading=false;
+                setState(() {
 
-    });
-    Navigator.pop(context);
-    }
-    catch (error)
-     {
-    isLoading=false;
-    setState(() {
+                });
+                print('Error:-${error.toString()}');
+              }
 
-    });
-    print('Error:-${error.toString()}');
-    }
-    
-  },backgroundColor: Colors.blue,child: Icon(Icons.search),)
+
+              // isLoading=true;
+              // setState(() {
+              //
+              // });
+              // await FirebaseFirestore.instance.collection('insert').add({
+              //   // key and value
+              //   'title': titleController.text,
+              //   'description':descriptionController.text
+              // }).then((onValue){
+              //   isLoading=false;
+              //   setState(() {
+              //
+              //   });
+              //   // success message------ snackbar-----
+              // }).onError((handleError,error){
+              //   print('Error----------------${handleError.toString()}');
+              //   isLoading=false;
+              //   setState(() {
+              //
+              //   });
+              // });
+            },
+            backgroundColor: Colors.blue,
+          )
         ],
       ),
     );
@@ -63,25 +94,22 @@ class _InsertscreenState extends State<Insertscreen> {
 class TextFieldWidget extends StatelessWidget {
   TextEditingController controller;
   String hintText;
-   TextFieldWidget({super.key,required this.controller, required this.hintText});
+  TextFieldWidget(
+      {super.key, required this.controller, required this.hintText});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin:  EdgeInsets.symmetric(horizontal: 18, vertical: 5),
+        margin: EdgeInsets.symmetric(horizontal: 18, vertical: 5),
         decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(10),
-
-        ),
+            color: Colors.blue, borderRadius: BorderRadius.circular(10)),
         child: TextFormField(
           controller: controller,
           style: TextStyle(color: Colors.white),
           decoration: InputDecoration(
-            border: InputBorder.none,
-            hintText: hintText,
-            hintStyle: TextStyle(color: Colors.white)
-          ),
+              border: InputBorder.none,
+              hintText: hintText,
+              hintStyle: TextStyle(color: Colors.white)),
         ));
   }
 }
--- lib/fetch_data.dart
+++ lib/fetch_data.dart
@@ -1,6 +1,8 @@
+import 'arci';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebasedemo/InsertScreen.dart';
 import 'package:flutter/material.dart';
+
 
 class FetchData extends StatefulWidget {
   const FetchData({super.key});
@@ -14,27 +16,25 @@ class _FetchDataState extends State<FetchData> {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(onPressed: (){
-        Navigator.push(context, MaterialPageRoute(builder: (context)=>Insertscreen()));
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>InsertScreen()));
       }),
       body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('insert').snapshots(),
-          builder: (context, snapshots) {
-            return ListView.builder(
-                itemCount: snapshots.data!.docs.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap: ()async{
-                      await FirebaseFirestore.instance.collection('insert').doc(snapshots.data!.docs[index]['docId'].toString()).delete();
-                    },
-                    title:
-                        Text(snapshots.data!.docs[index]['title'].toString()),
-                    subtitle: Text(
-                        snapshots.data!.docs[index]['description'].toString()),
-                    trailing: Text(snapshots.data!.docs[index]['docId'].toString()),
-                  );
-                });
-          }),
-
+        stream: FirebaseFirestore.instance.collection('insert').snapshots(),
+        builder: (context,snapshots){
+          return ListView.builder(
+              itemCount: snapshots.data!.docs.length,
+              itemBuilder: (context, index){
+                return ListTile(
+                  onTap: ()async{
+                    await FirebaseFirestore.instance.collection('insert').doc(snapshots.data!.docs[index]['docId'].toString()).delete();
+                  },
+                  title: Text(snapshots.data!.docs[index]['title'].toString()),
+                  subtitle: Text(snapshots.data!.docs[index]['description'].toString()),
+                  trailing: Text(snapshots.data!.docs[index]['docId'].toString()),
+                );
+              });
+        },
+      ),
     );
   }
 }
